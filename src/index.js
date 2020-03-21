cd9ac00a39195d14406554e107dfb098affb0455
phitranpt <phi.tran.pt@gmail.com>
2020-03-21 10:01:31 -0500

    added CommentDetail.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
-        <div>Hi there!</div>
+        <div className="ui container comments">
+            <CommentDetail author ="Sam"/>
+            <CommentDetail author ="Alex"/>
+            <CommentDetail author ="Jane"/>
+        </div>
     );
 };
 
